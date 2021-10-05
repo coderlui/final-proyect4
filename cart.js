e7aad8d26b014d458529f102521139aac672ca02
coderlui <lui.serna07@hotmail.com>
2021-10-04 23:27:51 -0500

    desafio clase 14 y 3ra entrega

---
+++ cart.js
@@ -0,0 +1,156 @@
+function shoppingCart() {
+
+    //Clase boton carro
+    const productToShoppingCart = document.querySelectorAll( '.botonCarro' ); 
+    productToShoppingCart.forEach( ( ACTButtons ) => {
+        ACTButtons.addEventListener( 'click', AtcBtnClick )
+    });
+    
+    function AtcBtnClick( event ) {
+        let btn = event.target;
+        const ejsProducts = btn.closest( '.card' );
+        
+
+        //obtener informacion del producto
+        const pdtImg = ejsProducts.querySelector( '.card-img-top' ).src;
+        const pdtTitle = ejsProducts.querySelector( '.card-title' ).textContent;
+        const prdPrice = ejsProducts.querySelector( '.card-price' ).textContent;
+        
+        modalCart( pdtImg, pdtTitle, prdPrice );
+        cartCounterUpdate();
+    };    
+    
+    const displayCart = document.querySelector( '.show-cart' );
+    function modalCart( pdtImg, pdtTitle, prdPrice ) {
+        
+        //evitar que se dupliquen productos
+        let ejsProductsTitleRepeat = displayCart.getElementsByClassName( 'shoppingCartPdtTitle' );
+        
+        for( let i = 0; i < ejsProductsTitleRepeat.length; i++ ) {
+            if( ejsProductsTitleRepeat[i].innerHTML === pdtTitle ) {
+                let ejsProductsTitleQuantity = ejsProductsTitleRepeat[i].parentElement.parentElement.querySelector( '.shoppingCartPdtQuantity' );
+                ejsProductsTitleQuantity.value++;
+                cartTotalPrice();
+                
+                return;
+            }
+        };
+        
+        
+        const shoppingCartDiv = document.createElement( 'div' );
+        const cartVisible =
+        `
+            <div class="row shoppingCartProdc mt-3 text-center">
+                <div class="col-3">
+                    <img src=${pdtImg} class="imagenesModal"/>
+                    <h6 class="mt-2 shoppingCartPdtTitle">${pdtTitle}</h6>
+                </div> 
+                
+                <div class="col-3">
+                    <p class="card-price shoppingCartProductPrice">${prdPrice}</p>
+                </div>
+                
+                <div class="col-3">
+                    <input class="text-center shoppingCartPdtQuantity inputCuenta" type="number" value="1">
+                </div>
+                  
+                <div class="col-3">
+                    <button class="btn btn-outline" id="remove-product-btn" data-name="${pdtTitle}">  x </button>
+                </div>
+            </div>
+        `
+        
+        shoppingCartDiv.innerHTML = cartVisible;
+        displayCart.append( shoppingCartDiv );
+        
+        // boton remover productos
+        
+        const removeButton = shoppingCartDiv.querySelector( '#remove-product-btn' );
+        
+        removeButton.addEventListener( 'click', deleteProductsInCart );
+        
+        //cantidades 
+        const inputCartQuantity = shoppingCartDiv.querySelector( '.shoppingCartPdtQuantity' );
+        
+        inputCartQuantity.addEventListener( 'change', cartQuantityChange );
+        
+        cartTotalPrice();
+    };
+
+    // contador 
+    
+    function cartCounterUpdate() {
+        
+        const cartProductsLength = document.querySelectorAll( '.shoppingCartProdc' );
+        const cartCounter = document.querySelector( '#cart-counter' );
+        cartCounter.innerHTML = cartProductsLength.length;
+        cartTotalPrice();
+    };  
+    
+    // precio total
+    
+    function cartTotalPrice() {
+        var totalCount = 0;
+        const totalPrice = document.querySelector( '.total-price' );
+        const shoppingCartProducts = document.querySelectorAll( '.shoppingCartProdc' );
+        
+        shoppingCartProducts.forEach( ( shoppingCartProdc ) => {
+            const pdtCartQtyEPriceElement = shoppingCartProdc.querySelector( '.shoppingCartProductPrice' );
+            const pCartPrice = Number( pdtCartQtyEPriceElement.textContent.replace( '$', '' ) );
+            
+            const pdtCartQtyElement = shoppingCartProdc.querySelector( '.shoppingCartPdtQuantity' );
+            const productInCartQuantity = Number( pdtCartQtyElement.value );
+            
+            totalCount += pCartPrice * productInCartQuantity;
+        });
+        
+        totalPrice.innerHTML = `$${totalCount.toFixed(2)}`;
+    };
+    
+    // eliminar productos del carro
+    
+    function deleteProductsInCart(event) {
+        const removeBtnClicked = event.target;
+        removeBtnClicked.closest( '.shoppingCartProdc' ).remove();
+        cartTotalPrice();
+        cartCounterUpdate();
+    };
+            
+    // qty de las unidades en el carrito
+    
+    function cartQuantityChange(event) {
+        const inputCartChange = event.target;
+        inputCartChange.value <= 0 ? ( inputCartChange.value = 1 ) : null;
+        cartTotalPrice();
+        cartCounterUpdate();
+    };
+
+    // Finalizar compra
+    
+    const checkoutButton = document.querySelector( '.btn-finalizar-compra' );
+    
+    checkoutButton.addEventListener('click', finalizarCompraTotal);
+    
+    function finalizarCompraTotal() {
+        displayCart.innerHTML = '';
+        cartTotalPrice();
+        cartCounterUpdate();
+    };
+
+    // Eliminar todos los productos del carro
+    
+    const removeCartElements = document.querySelector( '.btn-vaciar-carrito' );
+    
+    removeCartElements.addEventListener('click', removePdtsCart);
+    
+    function removePdtsCart() {
+        displayCart.innerHTML = '';
+        cartTotalPrice();
+        cartCounterUpdate();
+    };
+};
+    
+    shoppingCart();
+
+
+        
--- scriptindex.js
+++ scriptindex.js
@@ -1,4 +1,8 @@
-$(document).ready(function () {
+//DESAFIO CLASE 14 AJAX 
+
+
+
+$(document).ready(() => {
 
     $("h1").css("padding-top","50px");
     $("h1").css("color","#DDC3B6");
@@ -7,27 +11,17 @@ $(document).ready(function () {
     $("h2").css("padding-bottom","50px");
     $("#otraprueba").text("COMMING SOON");
 
-    function desafioJquery () {    
-        let stock = new Array;
-        let producto_Agregado = new Array;
-     
-        Producto_Creado("Shell Decor Necklace", 29900, "./images/img_9389.jpg", stock);
-        Producto_Creado("Pearl Necklace", 24900, "./images/IMG-0380.jpg", stock);
-        Producto_Creado("Mixed Charm Necklace", 29900, "./images/IMG-0381.jpg", stock);
-
-      
-        mostrarProductos(stock, producto_Agregado)
-    }
-    
+    //clases
     class Producto { 
-        constructor(nombre, precio, imgProducto) {
+        constructor(nombre, precio, imagen) {
             this.nombre = nombre;
             this.precio = precio;
-            this.imgProducto = imgProducto;
+            this.imagen = imagen;
         };
     };
 
-    class Producto_Seleccionado { 
+    //clases
+    class Seleccionado { 
         constructor(nombre, precio, cantidad) {
             this.nombre = nombre;
             this.precio = precio;
@@ -35,129 +29,212 @@ $(document).ready(function () {
         };
     };
 
-    function Producto_Creado(nombre, precio, imgProducto,stock){
-        stock.push(new Producto(nombre, precio, imgProducto));
-    };
-
-    function modificar_Carro(nombre, precio, producto_Agregado){
-        for(producto of producto_Agregado){
+    // función principal -visualización de información de las card 
+    function CardVisible () {   
+        let stock = new Array;
+        let carrito = new Array;        
+        function carroProductos(stock, carrito,) {
+        const jsonLocal = "./sass/archivoJSON.json"
+        $("producto_car").prepend('<div="producto_car"></div>');
+        $("#producto_car").click(() => { 
+        $.getJSON(jsonLocal, function ( respuesta, estado) {
+            console.log('respuesta')
+        if(estado === "success"){
+            let misDatos = respuesta;
+            for (const producto of misDatos) {
+                let info = document.createElement("div");
+                $(info).attr("id", `${producto.nombre}`);
+                $(info).html(` <div class="card h-80">
+                <img class="imagenProducto" src="${producto.imagen}"></img>
+                <h2 class="card-title centrar">${producto.nombre}</h2> 
+                <h2 class="card-title centrar">$${producto.precio}</h2>                            
+                                    </div>`);
+                $(info).addClass("producto");
+                $("#producto_carro").append(info);
+            stock.push (new Producto(producto.nombre,producto.precio,producto.imagen))
+            }  
+            $('.card-footer').on('click',()=>{agregar(event.target.parentElement.id, stock, carrito)});
+            
+        }    
+        });
+       
+    });    
+    
+}  
+    //interacción con HTML
+    carroProductos(stock, carrito)
+}
+ CardVisible();
+   
+    //Función de agregado 
+    function cambioCarrito(nombre, precio, carrito){
+        for(producto of carrito){
             if(producto.nombre == nombre) {
                 producto.cantidad += 1;
-                console.log(producto.cantidad)
-                return producto_Agregado;
+                return carrito;
             }
         }
-        producto_Agregado.push(new Producto_Seleccionado(nombre, precio, 1));
-        return producto_Agregado;
+            carrito.push(new Seleccionado(nombre, precio, 1));
+            return carrito;
+        };
+        
+    // función de agrgar Iva y confirmación 
+    function confirmacion_pro (total){
+        let confirmacion = confirm("¡Gracias por agregar este producto!");
+        let totalIva = 0;
+        if (confirmacion){
+            totalIva += (total * 1.21);
+            alert("El valor total a pagar con IVA es: $" + totalIva);
+        }
+        if (confirmacion)
+        {confirm ("¿Deseas agregar otro producto?");
+        }
     };
 
-    function mostrarProductos(stock, producto_Agregado) {
-        for (const producto of stock) {
-            let desafio = document.createElement("div");
-            $(desafio).attr("id", `${producto.nombre}`);
-            $(desafio).html(`<img class="imagenProducto" src="${producto.imgProducto}"></img>
-            <h2 class="card-title centrar">${producto.nombre}</h2>
-            <h2 class="card-title centrar">${producto.precio}</h2>`);
-            $(desafio).addClass("producto");
-            $("#proceso").append(desafio);
-            $(`[id='${producto.nombre}']`).append('<button class="botoncompra" id="luisita">Add to bag</button>');
+    //filtrado Productos     
+    function agregar(nuevoProducto,stock,carrito){
+        let busqueda = stock.find(producto => producto.nombre.toLowerCase(). includes(nuevoProducto.toLowerCase()));
+        alert("Seleccionaste " + busqueda.nombre + ", ¿Desea continuar?.");
+        carrito = cambioCarrito(busqueda.nombre, busqueda.precio, carrito);
+        let total = 0;
+        for (item of carrito) {
+            total += (item.precio * item.cantidad);
+        }
+        if(total) {
+            confirmacion_pro(total)
+        }
+    };
+                    
+        
+    //Efectos y animaciones 
+    
+    $(".col-sm-4").on("click", ()=>{
+        $("#clase13").fadeOut(5000)
+                    .fadeIn(1000);
+    });
+    
+    setInterval(function () {
+        $('h5',).fadeTo("fast" , 0.8).fadeTo("fast" , 0).delay(60).fadeTo("fast" , 1).delay(1000);}, 50);
+     
+});
+
+//DESAFIO CLASE 14 AJAX ----- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DESAFIO CLASE 13 ----- 
+
+
+
+$(document).ready(function () {
+    
+    function desafioJquery () {
+        let stock = new Array;
+        let producto_Agregado = new Array;
+        
+        
+        Producto_Creado("Shell Decor Necklace", 29900, "./images/img_9389.jpg", stock);
+        Producto_Creado("Pearl Necklace", 24900, "./images/IMG-0380.jpg", stock);
+        Producto_Creado("Mixed Charm Necklace", 29900, "./images/IMG-0381.jpg", stock);
+        
+        mostrarProductos(stock, producto_Agregado)
+    }
+    
+    class Producto {
+        
+        constructor(nombre, precio, imgProducto) {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.imgProducto = imgProducto;
         };
+    };
+
+     class Producto_Seleccionado { 
+         constructor(nombre, precio, cantidad) {
+             this.nombre = nombre;
+             this.precio = precio;
+             this.cantidad = cantidad;
+        };
+     };
+
+    function Producto_Creado(nombre, precio, imgProducto,stock){
+        stock.push(new Producto(nombre, precio, imgProducto));
+     };
+
+     function modificar_Carro(nombre, precio, producto_Agregado){
+         for(producto of producto_Agregado){
+           if(producto.nombre == nombre) {
+                 producto.cantidad += 1;
+                 console.log(producto.cantidad)
+                 return producto_Agregado;
+             }
+         }
+         producto_Agregado.push(new Producto_Seleccionado(nombre, precio, 1));
+         return producto_Agregado;
+     };
+
+     function mostrarProductos(stock, producto_Agregado) {
+         for (const producto of stock) {
+             let desafio = document.createElement("div");
+             $(desafio).attr("id", `${producto.nombre}`);
+             $(desafio).html(`<img class="imagenProducto" src="${producto.imgProducto}"></img>
+             <h2 class="card-title centrar">${producto.nombre}</h2>
+             <h2 class="card-title centrar">$${producto.precio}</h2>`);
+             $(desafio).addClass("producto");
+             $("#proceso").append(desafio);
+             $(`[id='${producto.nombre}']`).append('<button class="botoncompra">Add to bag</button>');
+         };
         $('.botoncompra').click(()=>{agregar_Prod(event.target.parentElement.id, stock, producto_Agregado)});
     }
 
-    function Prod_Agregado (total){
-        let confirmacion = confirm(" Gracias !!! Tu producto se a agregado al carrito");
-        if (confirmacion) {
-            alert("El valor total a pagar es $" + total);
-        } else {
-            alert("El valor total a pagar es $" + total);
+     function Prod_Agregado (total){
+         let confirmacion = confirm(" Gracias !!! Tu producto se a agregado al carrito");
+         if (confirmacion) {
+             alert("El valor total a pagar es $" + total);
+         } else {
+             alert("El valor total a pagar es $" + total);
         };
-    };
+     };
 
 
-    function agregar_Prod(nuevoProducto,stock,producto_Agregado){
-        let cantidad = prompt("Cuantas unidades deseas comprar?")
-        let busqueda = stock.find(producto => producto.nombre.toLowerCase().includes(nuevoProducto.toLowerCase()));
+     function agregar_Prod(nuevoProducto,stock,producto_Agregado){
+         let cantidad = prompt("Cuantas unidades deseas comprar?")
+         let busqueda = stock.find(producto => producto.nombre.toLowerCase().includes(nuevoProducto.toLowerCase()));
         alert("Haz elejido el producto " + busqueda.nombre + " y un total de " + cantidad + " unidades.");
         producto_Agregado = modificar_Carro(busqueda.nombre, busqueda.precio, producto_Agregado);
-        let total = 0;
-        for (item of producto_Agregado) {
-            total += (item.precio * cantidad);
-        }
+         let total = 0;
+         for (item of producto_Agregado) {
+             total += (item.precio * cantidad);
+         }
         if(total) {
             Prod_Agregado(total)
-        };
-    };
+         };
+     };
 
-    desafioJquery();
+     desafioJquery();
 
-    $("#intentos").click(function (){
-        alert("Producto: Prueba Pulsera. Valor $23900. Te interesa este producto? Dejanos tus datos en la pagina de contacto y en la seccion de comentarios escribe el nombre del producto y la cantidad que deseas. Tan pronto esté disponible en tienda fisica te lo haremos saber.")
-    });
+     $("#intentos").click(function (){
+         alert("Producto: Prueba Pulsera. Valor $23900. Te interesa este producto? Dejanos tus datos en la pagina de contacto y en la seccion de comentarios escribe el nombre del producto y la cantidad que deseas. Tan pronto esté disponible en tienda fisica te lo haremos saber.")
+     });
 
-    $("#intento1").click(function (){
+     $("#intento1").click(function (){
         alert("Producto: Prueba Aretes. Valor $21900. Te interesa este producto? Dejanos tus datos en la pagina de contacto y en la seccion de comentarios escribe el nombre del producto y la cantidad que deseas. Tan pronto esté disponible en tienda fisica te lo haremos saber.")
-    });
+     });
 
-    $("#intento2").click(function (){
-        alert("Producto: Prueba Bolso. Valor $55900. Si te interesa reservar este producto, llena los datos en la pagina de formulario y te haremos saber tan pronto esté disponible")
-    });
+     $("#intento2").click(function (){
+         alert("Producto: Prueba Bolso. Valor $55900. Si te interesa reservar este producto, llena los datos en la pagina de formulario y te haremos saber tan pronto esté disponible")
+     });
 
+ });
 
-    // EFECTOS - ANIMACIONES 
-
-
-    $(".col-sm-4").on("click", ()=>{
-
-        $("#clase13").fadeOut(5000)
-                     .fadeIn(1000);
-
-    });
-
-    setInterval(function () {
-        $('h5',).fadeTo("fast" , 0.8).fadeTo("fast" , 0).delay(60).fadeTo("fast" , 1).delay(1000);
-    }, 50);
-
-
-
-});
-
-
-
-
-
-// ejercicio index
-
-
-// class Necklaces {
-
-//     constructor(nombre, precio, stock) {
-
-//       this.nombre = nombre;
-//       this.precio = precio;
-//       this.stock = stock;
-//     }
-// }
-
-// let bestSellers0 = new Necklaces ("Shell Decor Necklace", 29900, 0 );
-// let bestSellers1 = new Necklaces ("Pearl Necklace" , 24900, 0 ) ;
-// let bestSellers2 = new Necklaces ("Mixed Charm Necklace", 29900, 0 );
-// let bestSellers3 = new Necklaces ("3pcs Necklace" , 19900, 0 ); 
-
-// let necklaces = [bestSellers0, bestSellers1, bestSellers2, bestSellers3];
-
-
-// let busqueda = prompt("Cual de nuestros best sellers es tu producto favorito?");
-
-// let respuesta = necklaces
-// .map((element) => {
-//     if(element.nombre === busqueda || element.precio === busqueda ) {
-//         element.stock--;
-//     } 
-
-//     return element;
-// })
-
-// console.log(respuesta);
-
-// alert("Ingresaste: " + busqueda + ". Muchas gracias por tu respuesta. Te haremos saber tan pronto tengamos este producto disponible").toUpperCase();
+//DESAFIO CLASE 13 ----- 
